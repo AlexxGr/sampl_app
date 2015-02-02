abc3b57200a14b10634dafcdbe153fef27f5a658
Alex Grib <alex.git@ukr.net>
2015-02-02 19:54:01 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ end
   def new
   	@user=User.new
   end
+
+# Действие create которое может обрабатывать проваьную регистрацию
+def create
+    @user = User.new(user_params)    
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+end
+
+private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
